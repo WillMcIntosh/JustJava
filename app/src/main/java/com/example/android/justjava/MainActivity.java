b7c5fa15a5bd58d6e660d430a415fe66cbd68a13
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-02-13 14:37:12 -0500

    added plus and minus buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -30,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
+    /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
